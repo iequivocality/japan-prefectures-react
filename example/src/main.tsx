14bbdd2be70b7617dea50d2ba09c0452191c952d
iequivocality <xanthiandere@gmail.com>
2024-12-09 20:03:25 +0800

    remove debug tool tanstack on example

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -7,7 +7,6 @@ import {
   createRoute,
   createRootRoute,
 } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import "./index.css";
 import { AppNav } from "./components/appnav";
 import { Home } from "./routes/Home";
@@ -23,7 +22,6 @@ const rootRoute = createRootRoute({
       <main className="font-sans dark:text-slate-200 flex flex-col items-center gap-y-16 min-w-screen py-8 min-h-screen">
         <Outlet />
       </main>
-      <TanStackRouterDevtools />
     </TooltipProvider>
   ),
 });
